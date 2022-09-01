eea1e61ca6c577dfb3e35a64c5629653b25a7565
Jeff Nfor <jeffnfor@gmail.com>
2022-08-31 20:51:52 -0400

    initial commit

--- vpc.tf
+++ vpc.tf
@@ -8,10 +8,10 @@
 resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
 
-  tags = tomap({
-    "Name", "terraform-eks-demo-node",
-    "kubernetes.io/cluster/${var.cluster-name}", "shared",
-})
+  //tags = tomap(
+    //"Name", "terraform-eks-demo-node",
+    //"kubernetes.io/cluster/${var.cluster-name}", "shared",
+   //)
 }
 
 resource "aws_subnet" "demo" {
@@ -22,10 +22,10 @@ resource "aws_subnet" "demo" {
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.demo.id
 
-  tags = tomap({
-    "Name", "terraform-eks-demo-node",
-    "kubernetes.io/cluster/${var.cluster-name}", "shared",
-})
+  //tags = tomap(
+    //"Name", "terraform-eks-demo-node",
+    //"kubernetes.io/cluster/${var.cluster-name}", "shared",
+ //)
 }
 
 resource "aws_internet_gateway" "demo" {
